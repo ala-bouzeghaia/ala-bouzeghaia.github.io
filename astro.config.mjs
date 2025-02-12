cf2d2a6b299bfb86bd6232bef6fb675f6a334e99
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-02-12 17:20:48 +0100

    feat: Add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        mdi: ["linkedin", "github", "email"],
+      },
+    }),
+  ],
 });
