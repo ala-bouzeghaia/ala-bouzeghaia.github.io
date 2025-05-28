b66785f6f65f675cd34f3f541402e54239760819
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-05-28 17:53:13 +0200

    feat: Add projects collection and include Pathfinding Visualizer project details

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,19 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    links: z.object({
+      website: z.string().url().optional(),
+      github: z.string().url(),
+    }),
+    thumbnail: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    date: z.coerce.date(),
+  }),
+});
+
+export const collections = { blog, projects };
